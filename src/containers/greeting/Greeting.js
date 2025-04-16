ca8e4daf467bd05b0b9c2984a5349f37f4b6d467
VTN <votungnghia1508@gmail.com>
2025-04-17 00:34:29 +0700

    edit3

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href="/ressss.png"
+                    href="/resume.png"
                     download="Resume.png"
                     className="download-link-button"
                   >
